190c1aeebacdbe91b560310dec257a8ef6e3d589
Harsh Patel <patelharsh241999@gmail.com>
2022-12-23 17:13:01 +0530

    * updated categories.

--- src/components/Categories/categories.jsx
+++ src/components/Categories/categories.jsx
@@ -1,53 +1,53 @@
 import React from "react";
 import Color from "color";
-import { makeStyles } from "@mui/styles";
+// import { makeStyles } from "@mui/styles";
 import { CardActionArea, Grid, Card, CardContent, CardMedia } from "@mui/material";
 
-const useGridStyles = makeStyles(({ breakpoints }) => ({
-  root: {
-    [breakpoints.up("md")]: {
-      justifyContent: "center",
-    },
-  },
-}));
+// const useGridStyles = makeStyles(({ breakpoints }) => ({
+//   root: {
+//     [breakpoints.up("md")]: {
+//       justifyContent: "center",
+//     },
+//   },
+// }));
 
-const useStyles = makeStyles(() => ({
-  actionArea: {
-    borderRadius: 16,
-    transition: "0.2s",
-    "&:hover": {
-      transform: "scale(1.1)",
-    },
-  },
-  card: ({ color }) => ({
-    minWidth: 256,
-    borderRadius: 16,
-    boxShadow: "none",
-    "&:hover": {
-      boxShadow: `0 6px 12px 0 ${Color(color).rotate(-12).darken(0.2).fade(0.5)}`,
-    },
-  }),
-  content: ({ color }) => {
-    return {
-      backgroundColor: color,
-      padding: "1rem 1.5rem 1.5rem",
-    };
-  },
-  title: {
-    fontFamily: "Keania One",
-    fontSize: "2rem",
-    color: "#fff",
-    textTransform: "uppercase",
-  },
-  subtitle: {
-    fontFamily: "Montserrat",
-    color: "#fff",
-    opacity: 0.87,
-    marginTop: "2rem",
-    fontWeight: 500,
-    fontSize: 14,
-  },
-}));
+// const useStyles = makeStyles(() => ({
+//   actionArea: {
+//     borderRadius: 16,
+//     transition: "0.2s",
+//     "&:hover": {
+//       transform: "scale(1.1)",
+//     },
+//   },
+//   card: ({ color }) => ({
+//     minWidth: 256,
+//     borderRadius: 16,
+//     boxShadow: "none",
+//     "&:hover": {
+//       boxShadow: `0 6px 12px 0 ${Color(color).rotate(-12).darken(0.2).fade(0.5)}`,
+//     },
+//   }),
+//   content: ({ color }) => {
+//     return {
+//       backgroundColor: color,
+//       padding: "1rem 1.5rem 1.5rem",
+//     };
+//   },
+//   title: {
+//     fontFamily: "Keania One",
+//     fontSize: "2rem",
+//     color: "#fff",
+//     textTransform: "uppercase",
+//   },
+//   subtitle: {
+//     fontFamily: "Montserrat",
+//     color: "#fff",
+//     opacity: 0.87,
+//     marginTop: "2rem",
+//     fontWeight: 500,
+//     fontSize: 14,
+//   },
+// }));
 
 const CustomCard = ({ classes, image, title, subtitle }) => {
   // const mediaStyles = useFourThreeCardMediaStyles();
