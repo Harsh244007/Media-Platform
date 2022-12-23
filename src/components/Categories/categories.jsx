ad40a0d527569d4483b6ad4c2d1329df378bafba
Harsh Patel <patelharsh241999@gmail.com>
2022-12-23 16:58:36 +0530

    * updated categories.

--- src/components/Categories/categories.jsx
+++ src/components/Categories/categories.jsx
@@ -1,12 +1,102 @@
-import { Card } from "@mui/material";
 import React from "react";
+import Color from "color";
+import GoogleFont from "react-google-font-loader";
+import { makeStyles } from "@material-ui/core/styles";
+import NoSsr from "@material-ui/core/NoSsr";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import { useFourThreeCardMediaStyles } from "@mui-treasury/styles/cardMedia/fourThree";
 
-const Categories = () => {
-    return (
-        <>
-        <Card></Card></>
-      );
-}
+const useGridStyles = makeStyles(({ breakpoints }) => ({
+  root: {
+    [breakpoints.up("md")]: {
+      justifyContent: "center",
+    },
+  },
+}));
 
+const useStyles = makeStyles(() => ({
+  actionArea: {
+    borderRadius: 16,
+    transition: "0.2s",
+    "&:hover": {
+      transform: "scale(1.1)",
+    },
+  },
+  card: ({ color }) => ({
+    minWidth: 256,
+    borderRadius: 16,
+    boxShadow: "none",
+    "&:hover": {
+      boxShadow: `0 6px 12px 0 ${Color(color).rotate(-12).darken(0.2).fade(0.5)}`,
+    },
+  }),
+  content: ({ color }) => {
+    return {
+      backgroundColor: color,
+      padding: "1rem 1.5rem 1.5rem",
+    };
+  },
+  title: {
+    fontFamily: "Keania One",
+    fontSize: "2rem",
+    color: "#fff",
+    textTransform: "uppercase",
+  },
+  subtitle: {
+    fontFamily: "Montserrat",
+    color: "#fff",
+    opacity: 0.87,
+    marginTop: "2rem",
+    fontWeight: 500,
+    fontSize: 14,
+  },
+}));
+
+const CustomCard = ({ classes, image, title, subtitle }) => {
+  const mediaStyles = useFourThreeCardMediaStyles();
+  return (
+    <CardActionArea className={classes.actionArea}>
+      <Card className={classes.card}>
+        <CardMedia classes={mediaStyles} image={image} />
+        <CardContent className={classes.content}>
+          <Typography className={classes.title} variant={"h2"}>
+            {title}
+          </Typography>
+          <Typography className={classes.subtitle}>{subtitle}</Typography>
+        </CardContent>
+      </Card>
+    </CardActionArea>
+  );
+};
+
+export const Categories = React.memo(function SolidGameCard() {
+  const gridStyles = useGridStyles();
+  const styles = useStyles({ color: "#203f52" });
+  const styles2 = useStyles({ color: "#4d137f" });
+  const styles3 = useStyles({ color: "#ff9900" });
+  const styles4 = useStyles({ color: "#34241e" });
+  return (
+    <>
+      <Grid classes={gridStyles} container spacing={4} wrap={"nowrap"}>
+        <Grid item>
+          <CustomCard classes={styles} title={"Inhouse Party"} subtitle={"Be a Legend!"} image={"https://picsum.photos/200/300"} />
+        </Grid>
+        <Grid item>
+          <CustomCard classes={styles2} title={"DJ"} subtitle={"Time to choose side!"} image={"https://picsum.photos/200/300"} />
+        </Grid>
+        <Grid item>
+          <CustomCard classes={styles3} title={"Weeding party"} subtitle={"What are you waiting?"} image={"https://picsum.photos/200/300"} />
+        </Grid>
+        <Grid item>
+          <CustomCard classes={styles4} title={"Pre Wedding"} subtitle={"Are you ready?"} image={"https://picsum.photos/200/300"} />
+        </Grid>
+      </Grid>
+    </>
+  );
+});
 export default Categories;
-<></>
